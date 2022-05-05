8dc45c2d9f6e1560ca63d5d5f5cd36f66faa0192
rubens Rodrigues <rubens@schoolguardian.app>
2022-05-04 23:39:47 -0300

    Aula 5 - Exec 2 - Autenticação

--- src/Screens/Login/LoginController.js
+++ src/Screens/Login/LoginController.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react'
-import LoginView from './LoginView'
+import React, { useState, useContext } from 'react';
+import useAPI from '../../Services/APIs/Common/useAPI';
+import auth from '../../Services/APIs/Auth/Auth';
+import LoginView from './LoginView';
+import { InfoContext } from '../../store/InfoContext';
 
-export default class LoginController extends Component {
+const LoginController = () => {
 
-  constructor() {
-    super();
-    this.state = {
-      isLoading: 0
-    };
+  const authLoginAPI = useAPI(auth.login);
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [connectMessage, setConnectMessage] = useState("");
+  const [connectCode, setConnectCode] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const context = useContext(InfoContext);
+
+
+  const onClickLogin = (values) => {
+    // navigate(-1);
+    console.log("values");
+    console.log(login);
+    console.log(password);
+
+    let infoSend = {
+      email: login,
+      password: password,
+    }
+
+    setIsLoading(true);
+    authLoginAPI.requestPromise(infoSend)
+      .then(info => {
+        setIsLoading(false);
+        context.onMakeLogin(info.token);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setConnectCode(-1);
+        console.log(error.data);
+        if (error.status === 401) {
+          setConnectMessage(error.data.message);
+        } else {
+          setConnectMessage("O servidor retornou um erro= " + error.status);
+        }
+
+      })
   }
 
-  onClickButton = () => {
   
-    this.setState({
-      isLoading: true
-    })
-  }
-
-  render() {
-    return (
-      <LoginView isLoading={this.state.isLoading} onClickButton={this.onClickButton} />
-    )
-  }
+  return (
+    <LoginView 
+      login={login}
+      setLogin={setLogin}
+      password={password}
+      setPassword={setPassword}
+      onClickLogin={onClickLogin}
+      isLoading={isLoading}
+      connectMessage={connectMessage}
+      connectCode={connectCode}/>
+  )
 }
+
+
+export default LoginController;
--- src/Screens/Login/LoginView.js
+++ src/Screens/Login/LoginView.js
@@ -5,12 +5,24 @@ import logoImg from '../../Images/Logo.png'
 import './Login.css'
 import CustomInput from '../../Components/CustomInput/CustomInput';
 
-const LoginView = ({ isLoading, onClickButton}) => {
+const LoginView = ({ onClickLogin, isLoading, login, password, setLogin, setPassword, connectMessage}) => {
 
-
-    let button = (<Button variant='primary' onClick={onClickButton} >Entrar</Button>);
-    if (isLoading){
-        button = (<CircularProgress size={25} className="circularProgress"/>)
+    let infoMessage = null;
+    let info = (<Button variant='primary' onClick={onClickLogin}>Entrar</Button>);
+    if(isLoading){
+        info = (
+            <div className='boxProgress'>
+                <CircularProgress color="primary" size={25}/>
+            </div>
+        )
+    } 
+    
+    if (connectMessage !== "") {        
+        infoMessage = (
+            <div className='infoErrorMessage'>
+                <Typography variant="minSize" color="error">{connectMessage}</Typography>
+            </div>
+        )        
     }
     return (
         <Grid container
@@ -21,32 +33,35 @@ const LoginView = ({ isLoading, onClickButton}) => {
             className='gridClass'>
             <Grid item md={6} sm={6} >
                 <Box className='boxImg'>
-                    <img src={childrenImg} alt="children" width={'60%'} className="childrenImg" />
+                    <img src={childrenImg} alt="children" width={'60%'} className="childrenImg"/>                    
                 </Box>
             </Grid>
             <Grid item md={6} sm={6}>
                 <Box className='boxLogin'>
-                    <img src={logoImg} alt="Logo" width={'40px'} className="logo" />
+                    <img src={logoImg} alt="Logo" width={'40px'} className="logo"/>
                     <Typography variant="h1">Bem-vindo!</Typography>
                     <CustomInput
                         label="Digite seu e-mail"
+                        defaultValue={login}
                         errorMessage=""
                         placeholder="mario@cqb.com.br"
-                        hasError={false}                        
-                    />
+                        hasError={false} 
+                        value={login}
+                        onChange={(event) => setLogin(event.target.value)}
+                        />
                     <CustomInput
                         label="Digite sua senha"
+                        defaultValue={password}
                         errorMessage=""
                         placeholder="**************"
                         hasError={false}
-                        type="password"
-                    />
-                    <div className='boxButton'>
-                        {button}
-                    </div>
-                    
-                </Box>
-            </Grid>
+                        type="password" 
+                        onChange={(event) => setPassword(event.target.value)}
+                        />
+                    {info}
+                    {infoMessage}
+                </Box>                
+            </Grid>            
         </Grid>
     );
 };
